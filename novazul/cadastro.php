08f04e9a6ebcbe9e580d41edb5f3f9754fad5f22
Isabella Caroline <isabellab.limaa2@gmail.com>
2025-11-10 11:27:57 -0300

    terminar

---
+++ novazul/cadastro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Bem vindo!</h2>
+    <h3>Agendamento para prevenção do cancêr de prostata</h3>
+    <label>Digite seu nome:</label>
+    <input type="text" name="nome" required><br><br>
+    <label>CPF:</label>
+    <input type="text" name="cpf"  minlength="11" maxlength="11" placeholder="Com 11 dígitos" required ><br><br>
+    <label>Idade:</label>
+    <input type="number" name="idade" minlength="2" maxlength="3" required><br><br>
+    <label>Data do atendimento:</label>
+    <input type="date" name="data" required ><br><br>
+
+
+    
+</body>
+</html>
